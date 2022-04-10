1d29eac8684131208202f33601ad234815588f1d
Valeria Lavin <valeriavlgc@gmail.com>
2022-04-10 19:35:12 +0200

    MovieDb 1.0

---
+++ src/main/resources/import.sql
@@ -0,0 +1,10 @@
+INSERT INTO FILM (ID, tmdb_id, title, overview, release_date, original_language) VALUES (1, 448366, "Building the Dream: Assembling the Avengers", "", "2012-09-25","en");
+INSERT INTO FILM (ID, tmdb_id, title, overview, release_date, original_language) VALUES (2, 487555, "The Avengers: Earths Mightiest Heroes Prelude", "", "2010-09-22", "en");
+INSERT INTO FILM (ID, tmdb_id, title, overview, release_date, original_language) VALUES (4, 550, "Fight Club", "", "1999-10-15", "en");
+INSERT INTO FILM (ID, tmdb_id, title, overview, release_date, original_language) VALUES (5, 671, "Harry Potter and the Philosophers Stone", "", "2001-11-16", "en");
+INSERT INTO FILM (ID, tmdb_id, title, overview, release_date, original_language ) VALUES (6, 672, "Harry Potter and the Chamber of Secrets", "","2002-11-13", "en");
+
+INSERT INTO TVSHOW (ID, first_air_date, tmdb_id, name, original_language, overview, popularity) VALUES (1, "2004-11-16", 1408, "House", "en", "", 308.492);
+
+INSERT INTO PERSON (ID, biography, birthday, gender, name, place_of_birth, popularity, profession, tmdb_id) VALUES (1, "", "1981-05-12", 2, "Rami Malek", "Torrance, California, USA", 22.399, "ACTOR", 17838);
+INSERT INTO PERSON (ID, biography, birthday, gender, name, place_of_birth, popularity, profession, tmdb_id) VALUES (2, "", NULL, 0, "DJ Head", NULL, 0.6, "ACTOR", 345);
